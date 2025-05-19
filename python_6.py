35323634c0ec5bbf4c5192417c73c9c1537c0f8e
sandhya <sandhyasign1@gmail.com>
2025-05-19 15:07:39 +0530

    added python_6.py files

---
+++ python_6.py
@@ -0,0 +1,20 @@
+ #Write a program that checks if a given string is a palindrome.
+# str=input("enter a string:")
+# if str==str[::-1]:
+#      print("string is palindrome")
+# else:
+#      print("string is not palindrome")
+#
+# ##Another method
+# def palindrome(string):
+#    return string == string[::-1]
+#
+# string=input("enter a string:")
+# palin = palindrome(string)
+# if palin:
+#     print("palindrome")
+# else:
+#     print("Not palindrome")
+#
+#
+# # math_utils.py
