ecadcd766943a6c44151cdb9a01f4920db13bfd6
sandhya <sandhyasign1@gmail.com>
2025-05-20 10:14:09 +0530

    add bubblesort.py

---
+++ tempconverter.py
@@ -0,0 +1,10 @@
+###2.Celsiustofahrenheit
+cel = float(input("enter a temp: "))
+fah = (cel * 9 / 5) + 32
+print(fah)
+
+##2.1.FahrenheittoCelsius
+fah = float(input("enter a temp: "))
+cel = (fah - 32) * 5 / 9
+print(cel)
+
